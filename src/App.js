274a3461675612580ab73c2a6e485cf395b245ce
Povindu Samarasekara <povinduchanmith@gmail.com>
2024-03-12 14:36:38 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 // pages & components
 import Home from './pages/Homepage/Homepage'
 import NavBar from './components/NavBar/NavBar';
+import Users from './pages/Users/Users';
 
 function App() {
   return (
@@ -14,6 +15,14 @@ function App() {
             <Route 
               path="/"
               element={<Home />}
+            />
+            <Route 
+              path="/doctors"
+              element={<Users type={"doctors"} />}
+            />
+            <Route 
+              path="/patients"
+              element={<Users type={"patients"} />}
             />
           </Routes>
         </div>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import './NavBar.css'
+import React from "react";
+import "./NavBar.css";
+import { Link } from "react-router-dom";
+
 export default function NavBar() {
   return (
-    <div className='NavBar-container'>
-        <ul className='NavList'>
-            <li className='NavList-Item'>Doctors</li>
-            <li className='NavList-Item'>Patients</li>
-            <li className='NavList-Item'>Reports</li>
-            <li className='NavList-Item'>Reports</li>
-        </ul>
+    <div className="NavBar-container">
+      <ul className="NavList">
+        <li className="NavList-Item">
+          <Link to="/doctors">Doctors</Link>
+        </li>
+        <li className="NavList-Item">
+          <Link to="/patients">Patients</Link>
+        </li>
+        <li className="NavList-Item">Reports</li>
+        <li className="NavList-Item">Reports</li>
+      </ul>
     </div>
-  )
+  );
 }
