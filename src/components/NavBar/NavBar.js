ff6c1e38b968c248649407eb66e495ae70e65e8e
Povindu Samarasekara <povinduchanmith@gmail.com>
2024-03-12 11:29:58 +0530

    Initial Setup

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './NavBar.css'
+export default function NavBar() {
+  return (
+    <div className='NavBar-container'>
+        <ul className='NavList'>
+            <li className='NavList-Item'>Doctors</li>
+            <li className='NavList-Item'>Patients</li>
+            <li className='NavList-Item'>Reports</li>
+            <li className='NavList-Item'>Reports</li>
+        </ul>
+    </div>
+  )
+}
