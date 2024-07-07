fd7597403ec39141507d561806365c27910d58fc
Povindu Samarasekara <povinduchanmith@gmail.com>
2024-07-07 22:36:55 +0530

    feat: refresh Access Token

--- src/services/RefreshAT.js
+++ src/services/RefreshAT.js
@@ -5,17 +5,25 @@ export const refreshAT = async (role, RT) => {
   const refreshToken = RT;
   console.log(refreshToken, role);
 
-  axios
+  const res = await axios
     .post(`${baseUrl}/refreshAT`, {
       refreshToken: refreshToken,
       role: role,
     })
     .then((res) => {
-      console.log("res", res);
-      return res;
+
+      console.log("res", res.data.token);
+
+      const user = JSON.parse(localStorage.getItem("user"));
+      user.token = res.data.token;
+      localStorage.setItem("user", JSON.stringify(user));
+
+      console.log(user);
+      return res.data.token;
     })
     .catch((error) => {
       console.log("error", error);
       return false;
     });
+  return res;
 };
